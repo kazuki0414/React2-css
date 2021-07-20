2d68a3f8fd341f7770b2c50dd8bd3639e309e0e5
kazuki0414 <78449556+kazuki0414@users.noreply.github.com>
2021-07-20 15:32:52 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+//コンポーネント Import
+import { InlineStyle } from "./components/InlineStyle";
+import { CssModules } from "./components/CssModules";
+import { StyledJsx } from "./components/StyledJsx";
+
+export default function App() {
+  return (
+    <div className="App">
+      <InlineStyle />
+      <CssModules />
+      <StyledJsx />
+    </div>
+  );
+}
---
+++ src/components/StyledJsx.jsx
@@ -0,0 +1,10 @@
+export const StyledJsx = () => {
+  return (
+    <>
+      <div className="container">
+        <p>- Styled JSX -</p>
+        <button>FIGHT!!</button>
+      </div>
+    </>
+  );
+};
